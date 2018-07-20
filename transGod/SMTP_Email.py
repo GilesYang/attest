ef0b059e92ecd37d60eb317ba6576760c0b3bf92
Giles <2134315476@qq.com>
2018-07-20 21:23:39 +0800

    smtp server/api update

--- transGod/SMTP_Email.py
+++ transGod/SMTP_Email.py
@@ -5,13 +5,14 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import os
+import datetime
 
 
 Exceptions = 'No Exception'
 def send_email(smtpHost, sendAddress, password, receiveAddress, subject='', content=''):
     msg = email.mime.multipart.MIMEMultipart()
     msg['from'] = sendAddress
-    msg['to'] = receiveAddress
+    msg['to'] = ''.join(receiveAddresses)
     msg['subject'] = subject
     content = content
     txt = email.mime.text.MIMEText(content, 'plain', 'utf-8')
@@ -33,9 +34,23 @@ def send_email(smtpHost, sendAddress, password, receiveAddress, subject='', cont
     smtp.sendmail(sendAddress, receiveAddress, str(msg))
     smtp.quit()
 try:
-    subject = 'python测试邮件'
-    content = '一封来自QA的测试报告邮件'
-    send_email('smtp.163.com', '15764512789@163.com', 'qwerqwer123456', 'yangjuntao@atman360.com', subject, content)
+    now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    subject = "Test_Report"
+    content = '一封来自QA杨俊涛的测试报告邮件.'+'Report Time:'+now_time
+    receiveAddresses = \
+        [
+            'yangjuntao@atman360.com',
+            'zhanglibin@atman360.com',
+            'liuwei@atman360.com',
+            'zbwyn@126.com',
+            'renfuyang@atman360.com',
+            'songkai@atman360.com',
+            'renlei@atman360.com',
+            'shaoshuai@atman360.com',
+            'zhangaihu@atman360.com',
+            'chenpengyu@atman360.com'
+        ]
+    send_email('smtp.163.com', '15764512789@163.com', 'qwerqwer123456', receiveAddresses, subject, content)
 except Exception as error:
     print(error)
 else:
